92aff3992e23aa93911b4dd4dc284e6bd129f652
rrs <contact@alligatr.co.uk>
2017-02-13 21:38:09 +0000

    added test endpoint

--- main.py
+++ main.py
@@ -4,10 +4,51 @@ import comic
 app = Flask(__name__)
 
 @app.route('/create', methods=['POST'])
-def index():
+def create():
   data = request.json
   title = data['title'] if 'title' in data else ''
   resp = make_response(comic.comic(title, data['messages']))
+  resp.headers['Content-Type'] = 'image/jpeg'
+  return resp
+
+@app.route('/test', methods=['GET'])
+def test():
+  data = {
+    "title": "test",
+    "messages": [
+      {
+        "user": "one",
+        "message": "he drinks a whiskey drink",
+        "timestamp": 1487018500
+      },
+      {
+        "user": "two",
+        "message": "he drinks a vodka drink",
+        "timestamp": 1487018500
+      },
+      {
+        "user": "one",
+        "message": "he drinks a lager drink",
+        "timestamp": 1487018500
+      },
+      {
+        "user": "two",
+        "message": "he drinks a cider drink",
+        "timestamp": 1487018500
+      },
+      {
+        "user": "three",
+        "message": "he sings the songs that remind him of the good times",
+        "timestamp": 1487018500
+      },
+      {
+        "user": "one",
+        "message": "he sings the songs that remind him of the better times",
+        "timestamp": 1487018500
+      },
+    ]
+  }
+  resp = make_response(comic.comic(data['title'], data['messages']))
   resp.headers['Content-Type'] = 'image/jpeg'
   return resp
 
